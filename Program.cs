e30aa2877493f842f8ef831bc51e90c33b737b67
Tobias Almada <talmada908@gmail.com>
2026-01-16 17:28:23 -0300

    Feature Swagger and Swagger ui added

--- Program.cs
+++ Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.MapGet("/", () => "Hello World!");
 app.Run();
