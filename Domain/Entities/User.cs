2f11d954fad9d987b79d46ff35b1f378fb2192ce
Tobias Almada <talmada908@gmail.com>
2026-01-16 17:19:48 -0300

    Add Swagger Package

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,1 +1,22 @@
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain;
+
+/*
+Campos mínimos:
+• Id (int, PK, identity/autoincrement)
+• Name (string, requerido)
+• Email (string, requerido, único)
+• IsActive (bool, por defecto true)
+• Password (hasheado)
+*/
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public bool IsActive { get; set; } = true;
+    public required string Password { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/wordpress", () => "Wordpress");
 app.Run();
