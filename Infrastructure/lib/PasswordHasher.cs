4c1332b6fbd65847c01f3c1a6e0c2fd26c1efa05
Tobias Almada <talmada908@gmail.com>
2026-01-18 01:45:08 -0300

    feature: Password Hasher with bcrypt

---
+++ Infrastructure/lib/PasswordHasher.cs
@@ -0,0 +1,13 @@
+namespace Infrastructure.lib;
+public class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 12);
+    }
+
+    public static bool VerifyPassword(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
